e78685570249163d11de156dfce5eefe42ef4116
tenthirtyone <alexsherbuck@gmail.com>
2020-06-06 03:10:01 -0400

    log prices

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const Logger = require("./lib/logger");
+const DB = require("./lib/db");
 const config = require("./lib/config");
 const Coinbase = require("./lib/coinbase");
 const Binance = require("./lib/binance");
@@ -8,22 +9,33 @@ const Bitstamp = require("./lib/bitstamp");
 class PriceMonitor {
   constructor() {
     this.config = config;
+    this.db = new DB(this.config.mongo);
     this.logger = new Logger("PriceMonitor");
-    this.coinbase = new Coinbase(this.config.coinbase);
-    this.binance = new Binance(this.config.binance);
-    this.bitstamp = new Bitstamp(this.config.bitstamp);
+    this.coinbase = new Coinbase(this.config.coinbase, this.db);
+    this.binance = new Binance(this.config.binance, this.db);
+    this.bitstamp = new Bitstamp(this.config.bitstamp, this.db);
   }
 
-  async init() {
-    this.coinbase.getPrices();
-    this.binance.getPrices();
-    this.bitstamp.getPrices();
+  async start() {
+    this.getPrices();
+
+    setInterval(() => {
+      this.getPrices();
+    }, this.config.interval);
+  }
+
+  async getPrices() {
+    const timestamp = new Date();
+
+    this.coinbase.getPrices(timestamp);
+    this.binance.getPrices(timestamp);
+    this.bitstamp.getPrices(timestamp);
   }
 }
 
 if (require.main === module) {
   const priceMonitor = new PriceMonitor();
-  priceMonitor.init();
+  priceMonitor.start();
 } else {
   module.exports = PriceMonitor;
 }
