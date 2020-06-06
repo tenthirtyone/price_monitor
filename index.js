af1020c4377189131869a99d04afe8b045e29eb8
tenthirtyone <alexsherbuck@gmail.com>
2020-06-06 01:33:31 -0400

    db and logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const Logger = require("./lib/logger");
 const config = require("./lib/config");
 const Coinbase = require("./lib/coinbase");
 const Binance = require("./lib/binance");
@@ -7,15 +8,16 @@ const Bitstamp = require("./lib/bitstamp");
 class PriceMonitor {
   constructor() {
     this.config = config;
+    this.logger = new Logger("PriceMonitor");
     this.coinbase = new Coinbase(this.config.coinbase);
     this.binance = new Binance(this.config.binance);
     this.bitstamp = new Bitstamp(this.config.bitstamp);
   }
 
   async init() {
-    //console.log(await this.coinbase.getSpotPrice());
-    //console.log(await this.binance.getSpotPrice());
-    console.log(await this.bitstamp.getSpotPrice());
+    this.coinbase.getPrices();
+    this.binance.getPrices();
+    this.bitstamp.getPrices();
   }
 }
 
