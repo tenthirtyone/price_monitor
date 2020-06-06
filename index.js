a09731fa490cfe4b5dfc7b08641cca77b3e9474b
tenthirtyone <alexsherbuck@gmail.com>
2020-06-05 22:21:36 -0400

    binance and bitstamp

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const config = require("./lib/config");
 const Coinbase = require("./lib/coinbase");
+const Binance = require("./lib/binance");
+const Bitstamp = require("./lib/bitstamp");
 
 class PriceMonitor {
   constructor() {
     this.config = config;
     this.coinbase = new Coinbase(this.config.coinbase);
-    this.
+    this.binance = new Binance(this.config.binance);
+    this.bitstamp = new Bitstamp(this.config.bitstamp);
   }
 
-  init() {}
+  async init() {
+    //console.log(await this.coinbase.getSpotPrice());
+    //console.log(await this.binance.getSpotPrice());
+    console.log(await this.bitstamp.getSpotPrice());
+  }
 }
 
 if (require.main === module) {
